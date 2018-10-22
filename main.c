01daca92ec48bd32098306ff1f22cc864a9ea476
karim mouaddel <karimmouaddel@MacBook-Pro-de-karim.local>
2018-10-22 16:21:29 -0400

    WIP

--- main.c
+++ main.c
@@ -1,7 +1,21 @@
+#include <stdlib.h>
 #include <stdio.h>
+#include <time.h>
+
 int main()
 {
-   // printf() displays the string inside quotation
-   printf("Hello, Frites!");
+   int game_tab[9][9];
+   char spacer1[42] = "++===+===+===++===+===+===++===+===+===++";
+   char spacer2[42] = "++---+---+---++---+---+---++---+---+---++";
+   printf("%s",&spacer1);
+   for(int i=0; i<9; i++){
+     for(int j=0; j<9; j++){
+       game_tab[i][j]=0;
+       printf("%d",game_tab[i][j]);
+     };
+   if ((i+1) %% 3) == 0{
+      printf("%s",&spacer2);
+    };
+  };
    return 0;
 }
