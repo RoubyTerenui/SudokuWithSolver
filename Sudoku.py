aac0396a3e7b8badcbccaf4229f5ae1e29939aaa
karim mouaddel <karimmouaddel@MacBook-Pro-de-karim.local>
2018-10-25 09:07:20 -0400

    Read and return list of sudoku with line number

--- Sudoku.py
+++ Sudoku.py
@@ -1,15 +1,28 @@
-def readSudoku(txtPath, sudokuNumber):
+def read_sudoku_in_line(txtPath, sudokuNumber):
+    #open sudoku txt file
     file = open(txtPath, "r")
-    sudokuList = file.readlines()
-    sudokuInLine = sudokuList[sudokuNumber]
-    sudokuToSolve = [];
-    for i in range(0,9):
-        sudokuTmp = []
-        for j in range (0,9):
-            sudokuTmp.append(sudokuInLine[j])
-            sudokuToSolve.append(sudokuTmp)
-    print(sudokuToSolve)
-    print(len(sudokuToSolve))
-    file.close()
 
-readSudoku("0.txt",0)
+    #Read all lines of this file
+    sudokuList = file.readlines()
+
+    #Get the disired sudoku
+    sudokuInLine    = sudokuList[sudokuNumber]
+    sudokuToSolve   = []
+    i = 0
+    while(i < len(sudokuInLine)):
+        sudokuTmp = []
+        sudokuTmp.append(sudokuInLine[i:i+9])
+        sudokuToSolve.append(sudokuTmp)
+        i = i + 9
+
+    #delete the last element
+    del sudokuToSolve[len(sudokuToSolve) - 1]
+    print(sudokuToSolve)
+    file.close()
+    return sudokuToSolve
+
+def is_line_valide(sudoku):
+    response = False
+
+
+List = read_sudoku_in_line("0.txt",0)
