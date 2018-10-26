9b172c53f52defd99d815669dddbecc0155aab97
Unknown <terenuirouby@live.fr>
2018-10-26 15:35:02 -0400

    WIP

--- Box.py
+++ Box.py
@@ -1,4 +1,8 @@
 class Box : #Class that defines a box of a sudoku
-    def _init_(self):
-        self.value=0
-        self.possibleValues=[1,2,3,4,5,6,7,8,9]
+    def __init__(self,value):
+        self.value=value
+        if value==0 :
+            self.possibleValues=[1,2,3,4,5,6,7,8,9]
+        else:
+            self.possibleValues=[value]
+    
--- Sudoku.py
+++ Sudoku.py
@@ -1,38 +1,41 @@
+from Box import *
 class Sudoku:
 
     def __init__(self):
         self.sudokuList = []
         self.sudokuToSolve = []
 
-<<<<<<< Updated upstream
+
     def read_sudoku_in_line(self, txtPath, sudokuNumber):
+
         #open sudoku txt file
         file = open(txtPath, "r")
-=======
-    #Get the disired sudoku
-    sudokuInLine = sudokuList[sudokuNumber]
-    sudokuToSolve = []
-    i = 0
-    while(i < len(sudokuInLine)):
-        sudokuTmp = []
-        sudokuTmp.append(sudokuInLine[i:i+9])
-        sudokuToSolve.append(sudokuTmp)
-        i = i + 9
->>>>>>> Stashed changes
-
         #Read all lines of this file
         self.sudokuList = file.readlines()
 
         #Get the disired sudoku
-        sudokuInLine    = self.sudokuList[sudokuNumber]
+        sudokuInLine   = self.sudokuList[sudokuNumber]
         i = 0
-        while(i < len(sudokuInLine)):
+        while(i < len(sudokuInLine)-1):
             sudokuTmp = []
-            sudokuTmp.append(sudokuInLine[i:i+9])
+            for j in range(0,9):
+                if (sudokuInLine[i+j])!="":
+                    box = Box(((int)(sudokuInLine[i+j])))
+                    #box.val
+                    sudokuTmp.append(box)
+            #sudokuTmp.append(sudokuInLine[i:i+9])
             self.sudokuToSolve.append(sudokuTmp)
             i = i + 9
-
         #delete the last element
-        del self.sudokuToSolve[len(self.sudokuToSolve) - 1]
+        #del self.sudokuToSolve[len(self.sudokuToSolve) - 1]
         file.close()
         return self.sudokuToSolve
+
+    def init_Possible_Value_Box(self,positionI,positionJ):#Position I(ligne) compris entre 0 et 8 # Position J(colonne) compris entre 0 et 8
+        if self.sudokuToSolve[positionI][positionJ].possibleValues.length()==1:
+            return
+        for j in range (0,9):
+            if j!=positionJ :
+                if  self.sudokuToSolve[positionI][j].possibleValues.length()==1:
+                    if self.sudokuToSolve[positionI][j].values in self.sudokuToSolve[positionI][positionJ].possibleValues :
+                        self.sudokuToSolve[positionI][positionJ].possibleValues.remove(self.sudokuToSolve[positionI][j].values)
