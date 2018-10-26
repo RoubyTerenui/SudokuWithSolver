082ca4b95eea260e0a963ee9d0caa843a92b8786
Unknown <terenuirouby@live.fr>
2018-10-26 13:48:28 -0400

    Merge Sudoku and push box

---
+++ Box.py
@@ -0,0 +1,4 @@
+class Box : #Class that defines a box of a sudoku
+    def _init_(self):
+        self.value=0
+        self.possibleValues=[1,2,3,4,5,6,7,8,9]
--- Sudoku.py
+++ Sudoku.py
@@ -4,9 +4,21 @@ class Sudoku:
         self.sudokuList = []
         self.sudokuToSolve = []
 
+<<<<<<< Updated upstream
     def read_sudoku_in_line(self, txtPath, sudokuNumber):
         #open sudoku txt file
         file = open(txtPath, "r")
+=======
+    #Get the disired sudoku
+    sudokuInLine = sudokuList[sudokuNumber]
+    sudokuToSolve = []
+    i = 0
+    while(i < len(sudokuInLine)):
+        sudokuTmp = []
+        sudokuTmp.append(sudokuInLine[i:i+9])
+        sudokuToSolve.append(sudokuTmp)
+        i = i + 9
+>>>>>>> Stashed changes
 
         #Read all lines of this file
         self.sudokuList = file.readlines()
