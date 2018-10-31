13102f5c0aa638503fdb741ae0f0d364b2617888
Unknown <terenuirouby@live.fr>
2018-10-31 18:37:56 -0400

    Test Completed

--- SudokuMain.py
+++ SudokuMain.py
@@ -3,11 +3,4 @@ from Sudoku import *
 sudoku = Sudoku()
 sudoku.read_sudoku_in_line("0.txt",0)
 sudoku.printSudoku()
-<<<<<<< Updated upstream
-=======
-
 sudoku.init_Constraint()
-
-
-
->>>>>>> Stashed changes
